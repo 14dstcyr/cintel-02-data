008ee81586de8dfb539d46cfe2b23885ae5cc901
Deb St. Cyr <156037147+14dstcyr@users.noreply.github.com>
2024-03-15 10:17:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
 import palmerpenguins  # This package provides the Palmer Penguins dataset
-
 import pandas as pd
 import seaborn as sns
 from shiny import reactive, render, req
